3bcca0990408e226b3917337f0f84cab7582ec8d
Manel <quemonmanel@gmail.com>
2022-10-03 19:09:19 +0200

    Practicas clase día 3 de Octubre JS

---
+++ js/main.js
@@ -0,0 +1,235 @@
+
+
+
+// console.log ("Hola mundo satanico");
+
+
+//soy un comentario de una linea
+/*hola soy un comentario que quiere ser de más de una línea*/
+/*
+let dia = "lunes";
+
+console.log (dia);
+
+dia="martes"; 
+
+console.log (dia);
+
+ variable vacia
+let dia; 
+
+let vacia = "";
+*/
+// let num= 666; 
+
+// console.log (num);
+
+// let encendido = false;
+
+// console.log (encendido); 
+
+// let texto = 'esto es la biblia'; 
+
+// console.log (texto); 
+
+// console.log (typeof(num)); // typeof es para que saque el tipo de dato que es 
+
+
+// let numero1 = 50; 
+
+// let numero2 = 73; 
+
+// let resultado = numero1 + numero2; 
+
+// console.log (resultado); 
+
+
+// let numero1 =11;
+
+// let numero2 = 22; 
+
+// let resultado = numero1 + 11; 
+
+// numero1 += 11; // esto es igual a numero1= +=11; 
+
+
+// let nombre = "jesus"; 
+
+// let edad = 26; 
+
+// if ((nombre === "Jesus") && (edad === 26))  {
+
+// console.log ("Que bien me cae este chico");
+
+// } else {
+
+//     console.log ("Ya no me cae bien"); 
+// }
+
+
+
+// let nombre = "jesus"; 
+
+// let edad = 22; 
+
+// if ((nombre === "Jesus") || (edad === 26))  {
+
+// console.log ("Que bien me cae este chico");
+
+// } else {
+
+//     console.log ("Ya no me cae bien"); 
+// }
+
+
+// let nombre = "jesus"; 
+
+// let edad = 26; 
+
+// if (nombre != "Ambrosio") {
+
+// console.log ("Bien"); 
+
+
+// } else {
+
+//     console.log ("Mal"); 
+// }
+
+
+// let tiempolibre = false; 
+
+// if (!tiempolibre) {
+
+
+//     console.log ("Que agonia"); 
+// }
+
+
+
+// let edad = 12; 
+
+// if (edad >= 18) {
+
+//     console.log ("Puedes pasar"); 
+
+// }else { 
+
+//     console.log ("No puedes ver la pelicula"); 
+// }
+
+
+// let numero = 18; 
+
+// let letra = "18";
+
+// if (numero === letra) {
+
+//     console.log ("Somos iguales"); 
+// }
+
+
+// let coche ="Renault"; 
+
+// if (coche === "Renault") {
+//     console.log ("Es un renault");
+// }else if (coche === "Seat") {
+
+//     console.log ("Es un seat"); 
+// }else  {
+
+// console.log ("Es otro coche"); 
+
+// }
+
+
+// let dia = "lunes"; 
+
+// switch(dia) {
+
+//  case "Lunes": 
+
+// console.log ("Es lunes....")
+//  break; 
+
+//  case "Martes":  
+// console.log ("Es martes.....")
+//  break; 
+//  case "Lunes": 
+
+
+//  break; 
+//  case "Lunes": 
+
+
+//  break; 
+
+//  case "Lunes": 
+
+
+//  break; 
+// default: 
+// console.log ("No es festivo"); 
+// break; 
+// }
+
+// switch (dia) {
+
+//     case Lunes:
+//         case Lunes:
+//             case Lunes:
+//                 case Lunes:
+//                     case Lunes:
+//                         console.log ("Es laboral...")
+    
+//                         break;
+
+//                         case Sabado:
+//                             case Domingo:
+//                                 console.log ("Es festivo"); 
+//                                 break;
+//                                 default:
+//                                     console.log ("no es ninguno de los anteriores")
+//                                     break; 
+// }
+
+let mes = "Junio"; 
+
+switch(mes) {
+
+    case "Diciembre":
+    case "Enero":
+    case "Febrero": 
+    case "Marzo":
+
+    console.log ("Es invierno"); 
+    break
+
+case "Abril":
+case "Mayo": 
+case "Junio":
+
+console.log ("Es primavera");
+break
+
+case "Julio":
+case "Agosto":
+case "Septiembre": 
+
+console.log ("Es verano");
+
+break
+
+case "Octubre":
+case "Noviembre":
+
+console.log ("Es otoño en el corte inglés"); 
+
+break
+
+default: 
+
+console.log ("No vives en este planeta"); 
+
+break;
+}
